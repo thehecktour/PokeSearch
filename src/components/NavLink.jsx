9f08f5ce691bff73e3ccc00656bd1de8ab02c263
1night <1nightshelter@gmail.com>
2024-11-14 10:44:12 +0000

    add page highlight

--- src/components/NavLink.jsx
+++ src/components/NavLink.jsx
@@ -1,8 +1,19 @@
 import { Link } from "react-router-dom";
 
-export default function Navlink({ id, children, link, updatePage }) {
+export default function Navlink({
+  id,
+  children,
+  link,
+  updatePage,
+  activePage,
+}) {
+  const isActive = activePage === id;
   return (
-    <li className="text-2xl inline mx-5 border-b-2 text-zinc-200 border-zinc-200">
+    <li
+      className={`text-2xl inline mx-5 ${
+        isActive && "border-b-2 text-zinc-200 border-zinc-200"
+      } `}
+    >
       <Link to={link} onClick={() => updatePage(id)}>
         {children}
       </Link>
--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -11,14 +11,29 @@ function Root() {
   return (
     <div className=" flex flex-col items-center min-h-screen  bg-zinc-900 text-zinc-200 font-serif">
       <ul className="mt-10 text-zinc-500">
-        <Navlink link={"/home"} id="home" updatePage={updatePage}>
+        <Navlink
+          link={"/home"}
+          id={"home"}
+          activePage={activePage}
+          updatePage={updatePage}
+        >
           Home
         </Navlink>
-        <Navlink link={"/search"} id="search" updatePage={updatePage}>
+        <Navlink
+          link={"/search"}
+          id={"search"}
+          activePage={activePage}
+          updatePage={updatePage}
+        >
           Search
         </Navlink>
-        <Navlink link={"/random"} id="random" updatePage={updatePage}>
-          Random {activePage}
+        <Navlink
+          link={"/random"}
+          id={"random"}
+          activePage={activePage}
+          updatePage={updatePage}
+        >
+          Random
         </Navlink>
       </ul>
       <Outlet />
