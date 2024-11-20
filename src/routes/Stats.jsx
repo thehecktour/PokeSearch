f5d0fe8e7d3f2b700b9a8f72cde71f3cf525fda2
Atilio <atilio.jesus@grupoboticario.com.br>
2024-11-20 19:39:08 -0300

    feat: ✨ add Stats.jsx with basic structure

---
+++ src/routes/Stats.jsx
@@ -0,0 +1,10 @@
+import { React, useState } from "react"
+
+export default function Stats(){
+    return(
+        <div>
+            <h1>Stats</h1>
+            <p>Coming soon...</p>
+        </div>
+    )
+}
